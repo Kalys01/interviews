ba9247022836a0d69afd8be2ca887361121d9fc8
kalys01 <kalysosmon96.g@gmail.com>
2024-04-28 02:12:47 +0600

    Created beauty header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-
+import AppHeader from '@/components/AppHeader.vue'
 </script>
 
 <template>
-  <RouterView />
+  <div class="container">
+    <AppHeader />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  width: 1280px;
+  margin: auto;
+  padding: 20px;
+}
+</style>
