a20b466fd7cac5c7e739eef63a82f2c145738948
kalys01 <kalysosmon96.g@gmail.com>
2024-04-28 02:11:10 +0600

    Imported via the arrow function for more security

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,13 +6,13 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: import('@/views/HomeView.vue')
+      component: () => import('@/views/HomeView.vue')
     },
     {
       path: '/about',
       name: 'about',
-      component: import('@/views/AboutView.vue')
-    },
+      component: () => import('@/views/AboutView.vue')
+    }
   ]
 })
 
