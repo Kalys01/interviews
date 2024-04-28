2d7e7b663d8b45ac1be33a56124f5f9f8b03e29f
kalys01 <kalysosmon96.g@gmail.com>
2024-04-29 00:35:04 +0600

    Added some kind of verification authorization

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,55 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw, RouteLocationNormalized, NavigationGuardNext } from 'vue-router'
+import { useUserStore } from '@/stores/user'
+
+const checkAuth = (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  const userUser = useUserStore()
+  if (!userUser.userId) {
+    next({ name: 'Auth' })
+  } else {
+    next()
+  }
+}
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/PageHome.vue'),
+    beforeEnter: checkAuth
+  },
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: () => import('@/views/PageAuth.vue')
+  },
+  {
+    path: '/interview:id',
+    name: 'Interview',
+    component: () => import('@/views/PageInterview.vue'),
+    beforeEnter: checkAuth
+  },
+  {
+    path: '/list',
+    name: 'List',
+    component: () => import('@/views/PageList.vue'),
+    beforeEnter: checkAuth
+  },
+  {
+    path: '/statistic',
+    name: 'Statistic',
+    component: () => import('@/views/PageStatistic.vue'),
+    beforeEnter: checkAuth
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/views/AboutView.vue')
-    }
-  ]
+  routes: routes
 })
 
 export default router
