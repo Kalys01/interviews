ebf551d9fa38244015d19aa0e2d6ac89d169bab2
kalys01 <kalysosmon96.g@gmail.com>
2024-05-03 02:51:36 +0600

    I took it out of the database and made the delete button

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,18 @@ import { initializeApp } from 'firebase/app'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import Toast from 'primevue/toast'
+import ConfirmationService from 'primevue/confirmationservice'
 import Menubar from 'primevue/menubar'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
-import Toast from 'primevue/toast'
 import ProgressSpinner from 'primevue/progressspinner'
 import ToastService from 'primevue/toastservice'
 import Card from 'primevue/card'
 import DataTable from 'primevue/datatable'
-import Column from 'primevue/Column'
+import Column from 'primevue/column'
+import ConfirmDialog from 'primevue/confirmdialog'
+import InlineMessage from 'primevue/inlinemessage'
 
 import App from './App.vue'
 import router from './router'
@@ -35,9 +38,10 @@ initializeApp(firebaseConfig)
 
 const app = createApp(App)
 
-app.use(ToastService)
 app.use(createPinia())
 app.use(router)
+app.use(ToastService)
+app.use(ConfirmationService)
 app.use(PrimeVue)
 app.component('app-menubar', Menubar)
 app.component('app-button', Button)
@@ -47,5 +51,7 @@ app.component('app-progress-spinner', ProgressSpinner)
 app.component('app-card', Card)
 app.component('app-datatable', DataTable)
 app.component('app-column', Column)
+app.component('app-dialog', ConfirmDialog)
+app.component('app-message', InlineMessage)
 
 app.mount('#app')
